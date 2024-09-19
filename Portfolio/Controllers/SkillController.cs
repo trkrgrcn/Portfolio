d3ad2b17ce85b69fbd2d92a05222ec0fa3e7df7c
Türker GÜRCAN <turker.gurcan@csb.gov.tr>
2024-09-19 10:35:34 +0300

    Skill Add-Delete

---
+++ Portfolio/Controllers/SkillController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Portfolio.Models;
+namespace Portfolio.Controllers
+{
+    public class SkillController : Controller
+    {
+     MyPortfolioDbEntities1 _context = new MyPortfolioDbEntities1();
+        public ActionResult SkillList()
+        {
+            var values = _context.Skill.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateSkill()
+        { 
+            return View(); 
+        }
+        [HttpPost]
+        public ActionResult CreateSkill(Skill skill)
+        { 
+            _context.Skill.Add(skill);
+            _context.SaveChanges();
+            return RedirectToAction("SkillList"); 
+        }   
+        
+        public ActionResult DeleteSkill(int id)
+        {
+            var value = _context.Skill.Find(id);
+            _context.Skill.Remove(value);
+            _context.SaveChanges();
+            return RedirectToAction("SkillList"); 
+        }
+
+    }
+}
