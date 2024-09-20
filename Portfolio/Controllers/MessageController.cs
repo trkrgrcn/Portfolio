414e8035b047c4729797d41658a602d649d570b8
Türker GÜRCAN <turker.gurcan@csb.gov.tr>
2024-09-20 23:51:53 +0300

    5.Ders

---
+++ Portfolio/Controllers/MessageController.cs
@@ -0,0 +1,39 @@
+﻿using Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class MessageController : Controller
+    {
+        MyPortfolioDbEntities1 _context = new MyPortfolioDbEntities1();
+        // GET: Message
+        public ActionResult Inbox()
+        {
+            var values = _context.Message.ToList();
+            return View(values);
+        }
+        public ActionResult MessageDetails(int id) 
+        { 
+        var value = _context.Message.Where(x=>x.MessageId==id).FirstOrDefault();
+            return View(value);
+        }
+        public ActionResult MesaageStatusChange(int id)
+        {
+            var value = _context.Message.Where(x => x.MessageId == id).FirstOrDefault();
+            if(value.IsRead==false)
+            {
+                value.IsRead = true;
+            }
+            else
+            {
+                value.IsRead = false;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+    }
+}
